d4265f121d3b2424cb7f6a4af806ba5855961752
na1tto <eduardo3245.ss@gmail.com>
2026-08-18 09:03:55 -0300

    feat: added stock service preliminar structure

---
+++ cmd/stock-service/errors.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"net/http"
+)
+
+// handling errors internaly through application functions
+// doing so prevents the user to have access to internal application
+// informations, like the stacktree from the error
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	app.logger.Errorw("internal error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
+
+	writeJsonError(w, http.StatusInternalServerError, "the server encountered a problem")
+}
+
+func (app *application) forbiddenResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logger.Warnw("forbidden", "method", r.Method, "path", r.URL.Path, "error", err.Error())
+
+	writeJsonError(w, http.StatusForbidden, "forbidden")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logger.Warnw("bad request", "method", r.Method, "path", r.URL.Path, "error", err.Error())
+
+	writeJsonError(w, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logger.Warnf("not found error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
+
+	writeJsonError(w, http.StatusNotFound, "not found")
+}
+
+func (app *application) conflictResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logger.Errorw("conflict response", "method", r.Method, "path", r.URL.Path, "error", err.Error())
+
+	writeJsonError(w, http.StatusConflict, err.Error())
+}
+
+func (app *application) unauthorizedErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logger.Warnw("unauthorized error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
+
+	writeJsonError(w, http.StatusUnauthorized, "unauthorized")
+}
+
+func (app *application) unauthorizedBasicErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logger.Warnw("unauthorized basic error", "method", r.Method, "path", r.URL.Path, "error", err.Error())
+
+	w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+
+	writeJsonError(w, http.StatusUnauthorized, "unauthorized")
+}
+
+func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request, retryAfter string) {
+	app.logger.Warnw("rate limit  exceeded", "method", r.Method, "path", r.URL.Path)
+
+	w.Header().Set("Retry-After", retryAfter)
+
+	writeJsonError(w, http.StatusTooManyRequests, "rate limit exceeded, retry after: "+retryAfter)
+}
---
+++ cmd/stock-service/json.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var Validate *validator.Validate
+
+func init() {
+	Validate = validator.New(validator.WithRequiredStructEnabled())
+}
+
+func writeJson(w http.ResponseWriter, status int, data any) error {
+	if status == http.StatusNoContent {
+		w.WriteHeader(status)
+		return nil
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func readJson(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1_048_578 // maximum of ONE megabyte for the request
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	return decoder.Decode(data)
+}
+
+// we want to return our errors in the json format as well
+func writeJsonError(w http.ResponseWriter, status int, message string) error {
+	type envelope struct {
+		Error string `json:"error"`
+	}
+
+	return writeJson(w, status, &envelope{Error: message})
+}
+
+// this is a standarized way of returning json responses across our application
+// this abstracts the writeJson method by allowing it to return any type of data
+// in this way all of the data in the response will be inside a "data" value
+// we did the same thing at the errors.go package for standarazing error responses
+func (app *application) jsonResponse(w http.ResponseWriter, status int, data any) error {
+	type envelope struct {
+		Data any `json:"data"`
+	}
+
+	return writeJson(w, status, &envelope{Data: data})
+}
