abba3ba53afbc11ff5ca071869a30a539c9ae202
na1tto <eduardo3245.ss@gmail.com>
2026-08-23 00:09:09 -0300

    fix: updated prod api routes prefix and disabled prerender

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  stockApiUrl: 'https://stock-service-659661883921.us-east1.run.app',
-  invoicingApiUrl: 'https://invoicing-service-659661883921.us-east1.run.app'
+  stockApiUrl: 'https://stock-service-659661883921.us-east1.run.app/v1',
+  invoicingApiUrl: 'https://invoicing-service-659661883921.us-east1.run.app/v1'
 }
